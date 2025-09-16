25fef579e7b63ee2ea9852f8954a1a414b04b32c
Sreenu <sreenugod2001@gmail.com>
2025-09-16 09:49:23 +0530

    Ai chat upload form file is created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import AIChatUploadform from './components/AIChatUploadform';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <h2>AI Chat Assistant </h2>
+      <i>--For Interview Question Generation ...</i>
+      <AIChatUploadform />
     </div>
   );
 }
---
+++ src/components/AIChatUploadform.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AIChatUploadform = () => {
+  return (
+    <div>AIChatUploadform</div>
+  )
+}
+
+export default AIChatUploadform
