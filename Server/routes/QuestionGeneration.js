ec7494fda70f8dc69341f1f73636a957d3740573
Sreenu <sreenugod2001@gmail.com>
2025-09-17 16:27:33 +0530

    integrated openai api

--- Server/routes/QuestionGeneration.js
+++ Server/routes/QuestionGeneration.js
@@ -1,16 +1,24 @@
 const express = require("express");
+const { callOpenAIGenerateQuestion } = require("../Services/OpenAI");
 
 const router = express.Router();
 
 router.post("/generate", async (request, res, next) => {
     const { jobDescription, resume } = request.body
-    console.log(jobDescription, resume)
-    // try {
-    //     // const response = await callOpenAIGenerateQuestion()
-    //     // res.send("Interview question will generate coming soon...")
-    // } catch (error) {
-    //     next(error)
-    // }
+    const prompt = `
+  You are an interview assistant. Based on this Resume/Job Description:
+  ${resume || jobDescription}
+
+
+  Generate 5 interview questions with short sample answers.
+  `;
+    try {
+        const response = await callOpenAIGenerateQuestion(prompt)
+        console.log(response)
+        res.send("Interview question will generate coming soon...")
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router
