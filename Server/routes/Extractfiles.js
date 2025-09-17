fa30f2724c5445a9f98fd58b82b640ff460b11d1
Sreenu <sreenugod2001@gmail.com>
2025-09-17 16:25:55 +0530

    Pdf and docs extraction added.

--- Server/routes/Extractfiles.js
+++ Server/routes/Extractfiles.js
@@ -2,16 +2,46 @@
 const express = require("express");
 const multer = require("multer");
 const pdfParse = require("pdf-parse");
+const docsParse = require("mammoth")
 const router = express.Router();
 
-const upload = multer({ dest: "uploads/" });
+
+// multer() tells Express: “Hey, I want to accept files from forms (like resumes, images, PDFs, etc.).”
+
+// { dest: "uploads/" } → means store the uploaded files temporarily in a folder called uploads / on your server.
+// const upload = multer({ dest: "uploads/" });
+
+
+//Memory storage → instead of saving to disk, keep the file in memory buffer:
+//This way, the file isn’t written to disk, but you can directly read it from req.file.buffer
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+
 
 router.post("/extract-files", upload.single("uploadfile"), async (req, res) => {
-    console.log(req.file, "req")
     try {
-        const dataBuffer = req.file;
-        const pdfData = await pdfParse(dataBuffer);
-        res.json({ text: pdfData.text });
+        let text = "";
+        const resumeFile = req.file;
+        console.log(req.file)
+        if (resumeFile.mimetype === "application/pdf") {
+            const pdfData = await pdfParse(resumeFile.buffer);
+            text = pdfData.text;
+        } else if (resumeFile.mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+            const docData = await docsParse.extractRawText({ buffer: resumeFile.buffer })
+            text = docData.value;
+        } else {
+            res.status(400).json({ error: "Unsupported file type" })
+        }
+        res.json({ text })
+        // res.json({
+        //     // id: path.basename(req.file.filename, path.extname(req.file.filename)),
+        //     filename: req.file.originalname,
+        //     storedFilename: req.file.filename,
+        //     mimetype: req.file.mimetype,
+        //     size: req.file.size,
+        //     uploadTime: Date.now()
+        // });
     } catch (err) {
         console.log(err.message, "errrpr")
         res.status(500).json({ error: err.message });
